19c110bc1c04c80e7116b99dc48fcd913a47da82
yoyobaba1506 <rajanikants1506@gmail.com>
2023-10-30 00:39:43 +0530

    Updated Cors

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(
     //     allowedHeaders:"Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"
     // }
     )
-)
 app.use('/api', apiRoutes);
 
 
