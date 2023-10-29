d94436d40f5a37cbb8ee682415e31149b0d29135
yoyobaba1506 <rajanikants1506@gmail.com>
2023-10-30 01:21:33 +0530

    Changed Cors

--- server.js
+++ server.js
@@ -13,13 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(
-    cors(
-        {
-        origin:'https://yoyoauth-app.vercel.app/',
+    cors({
+        origin:'https://yoyoauth-app.vercel.app',
         //origin:'*',
         credentials:true,
-        optionSuccessStatus:200,
-        preflightContinue:false,
+        // optionSuccessStatus:200,
+        // preflightContinue:false,
         //allowedHeaders:"Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"
     }
     )
