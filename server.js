e191249afce624f46fef08201ec07b45f36a891f
yoyobaba1506 <rajanikants1506@gmail.com>
2023-10-29 21:36:30 +0530

    Changed the Varcel URL

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(
     cors({
-        origin:'http://localhost:3000',
+        origin:['http://localhost:3000','https://yoyoauth-app.varcel.app'],
         credentials:true,
         optionSuccessStatus:200
     })
