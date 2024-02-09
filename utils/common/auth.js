7470c51025f1bbc11d878605d7f94ef26c25a313
Rajani Kant Sharma <rajanikants1506@gmail.com>
2024-02-10 01:50:39 +0530

    Updated JWT

--- utils/common/auth.js
+++ utils/common/auth.js
@@ -3,9 +3,9 @@ const jwt = require("jsonwebtoken");
 const { ServerConfig } = require("../../config");
 const serverConfig = require("../../config/server-config");
 
-async function createToken(input) {
+function createToken(input) {
   try {
-    const token = await jwt.sign(input, serverConfig.JWT_SECRET, {
+    const token = jwt.sign(input, serverConfig.JWT_SECRET, {
       expiresIn: serverConfig.JWT_EXPIRY,
     });
     return token;
@@ -17,18 +17,17 @@ async function createToken(input) {
 
 function verifyToken(token) {
   try {
-    return jwt.verify(token, "adsfdhdj%$#@*!1285mkl");
+    return jwt.verify(token, ServerConfig.JWT_SECRET);
   } catch (error) {
     console.log(error);
     throw error;
   }
 }
 
-async function checkPassword(plainPassword, encryptedPassword) {
+function checkPassword(plainPassword, encryptedPassword) {
   try {
-    return await bcrypt.compare(plainPassword, encryptedPassword);
+    return bcrypt.compareSync(plainPassword, encryptedPassword);
   } catch (error) {
-    console.log(error);
     throw error;
   }
 }
